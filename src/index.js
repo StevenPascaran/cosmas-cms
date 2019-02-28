d7057a7593f51b6e343ef207b03d6c77d0b869ab
Odaymon <stevenpascaran@gmail.com>
2019-03-01 06:15:36 +0800

    sdfg

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(__dirname + '/'));
+app.get('*', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
+
+
 import AddDrug from './scripts/adddrug';
 import DeleteDrug from './scripts/deletedrug';
 import UpdateDrug from './scripts/updatedrug';
