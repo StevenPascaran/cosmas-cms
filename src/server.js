6b23470d2dd98fd14ee9dd5f707386a6d99882a1
Odaymon <stevenpascaran@gmail.com>
2019-03-01 16:21:48 +0800

    revert to original

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 8080
 
-express().get('../index', function (req, res) {
-	res.send('hello')
+express().all('/index', function (req, res) {
+	console.log('Accessing the secret section ...')
 })
 
 express().listen(PORT, () => console.log(`Listening on ${ PORT }`))
