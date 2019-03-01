8c654f95a910efc6fff0c4ae423994324b79acb9
Odaymon <stevenpascaran@gmail.com>
2019-03-01 16:11:08 +0800

    revert to original

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 8080
 
-express().get('/', function (req, res) {
-	res.send('index')
+express().get('/index', function (req, res) {
+	res.send('hello')
 })
+
 express().listen(PORT, () => console.log(`Listening on ${ PORT }`))
