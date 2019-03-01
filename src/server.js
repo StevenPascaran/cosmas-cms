833edad6129b566e3030fcf885711aaecb22844d
Odaymon <stevenpascaran@gmail.com>
2019-03-01 16:03:08 +0800

    server.js updated

--- src/server.js
+++ src/server.js
@@ -2,10 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 8080
 
-express()
-//.use(express.static(path.join(__dirname, 'src/styles')))
-//.set('view engine', 'ejs')
-.get('/', (req, res) {
+express().get('/', function (req, res) {
 	res.send('index')
 })
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+express.listen(PORT, () => console.log(`Listening on ${ PORT }`))
