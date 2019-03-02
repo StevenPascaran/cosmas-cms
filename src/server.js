58655b97adc7a7a57dcdd627e8e74a086e4615d9
Odaymon <stevenpascaran@gmail.com>
2019-03-03 00:06:26 +0800

    updated package and server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const path = require('path');
 const PORT = process.env.PORT || 8080;
+const path = require('path');
 
 express().get('/', (req, res) => {
   res.send('root route');
