8be3429deca8ff61e8a70d4a1c160a6f736eac77
Odaymon <stevenpascaran@gmail.com>
2019-03-02 23:07:10 +0800

    server.js updated

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 8080;
 
-express.get('/', (req, res) => {
+express().get('/', (req, res) => {
   res.send('root route');
 });
 
-express.listen(PORT, (res,req) => {
+express().listen(PORT, (res,req) => {
 	console.log(`Listening on ${ PORT }`)
 });
