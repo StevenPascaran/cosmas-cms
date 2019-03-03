e7cdd85673f731634ebf84dcc12a7559e8d24991
Odaymon <stevenpascaran@gmail.com>
2019-03-03 12:40:03 +0800

    testing server.js

--- src/server.js
+++ src/server.js
@@ -1,43 +1,14 @@
 const express = require('express');
+const server = express();
 const PORT = process.env.PORT || 8080;
 //const path = require('path');
 
-//express().get('/', (req, res) => {
-//  res.send('root route');
-//});
+server.set('port', process.env.PORT || 3000);
 
-const http = require('http'),
-      //server = http.createServer(),
-	  url = require('url');
-
-makeServer = function (request,response){
-   let path = url.parse(request.url).pathname;
-   console.log(path);
-   if(path === '/'){
-      response.writeHead(200,{'Content-Type':'text/plain'});
-      response.write('Hello world');
-   }
-   else if(path === '/about'){
-     response.writeHead(200,{'Content-Type':'text/plain'});
-     response.write('About page');
-   }
-   else if(path === '/blog'){
-     response.writeHead(200,{'Content-Type':'text/plain'});
-     response.write('Blog page');
-   }
-   else{
-     response.writeHead(404,{'Content-Type':'text/plain'});
-     response.write('Error page');
-   }
-   response.end();
- },
- 
-server = http.createServer(makeServer);
-
-server.listen(PORT, (res,req) => {
-	console.log(`Listening on ${ PORT }`)
+server.get('/', (req, res)=>{
+  res.send('Home Page');
 });
 
-//express().listen(PORT, (res,req) => {
-//	console.log(`Listening on ${ PORT }`)
-//});
+server.listen(PORT, (res,req)=>{
+	console.log(`Listening on ${ PORT }`)
+});
