b0cc36ef269ab71f053a0268b46c312a12be32e6
Odaymon <stevenpascaran@gmail.com>
2019-03-03 12:45:50 +0800

    testing server.js

--- src/server.js
+++ src/server.js
@@ -9,6 +9,17 @@ server.get('/', (req, res)=>{
   res.send('Home Page');
 });
 
+server.get('/about',(req,res)=>{
+   response.send('About page');
+});
+
+//Express error handling middleware
+server.use((req,res)=>{
+   response.type('text/plain');
+   response.status(505);
+   response.send('Error page');
+});
+
 server.listen(PORT, (res,req)=>{
 	console.log(`Listening on ${ PORT }`)
 });
