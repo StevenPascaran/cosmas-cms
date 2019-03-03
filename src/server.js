9982447a7352ccc44bd97fa9ca9ff018ef563b26
Odaymon <stevenpascaran@gmail.com>
2019-03-03 12:25:45 +0800

    testing server.js

--- src/server.js
+++ src/server.js
@@ -10,11 +10,29 @@ const http = require('http'),
       server = http.createServer(),
 	  url = require('url');
 
-server.on('request',(request,response)=>{
-   response.writeHead(200,{'Content-Type':'text/plain'});
-   response.write('Hello world');
+makeServer = function (request,response){
+   let path = url.parse(request.url).pathname;
+   console.log(path);
+   if(path === '/'){
+      response.writeHead(200,{'Content-Type':'text/plain'});
+      response.write('Hello world');
+   }
+   else if(path === '/about'){
+     response.writeHead(200,{'Content-Type':'text/plain'});
+     response.write('About page');
+   }
+   else if(path === '/blog'){
+     response.writeHead(200,{'Content-Type':'text/plain'});
+     response.write('Blog page');
+   }
+   else{
+     response.writeHead(404,{'Content-Type':'text/plain'});
+     response.write('Error page');
+   }
    response.end();
-});
+ },
+ 
+server = http.createServer(makeServer);
 
 server.listen(PORT, (res,req) => {
 	console.log(`Listening on ${ PORT }`)
