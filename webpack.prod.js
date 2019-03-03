dfd131f16e810d9a887b38f2d8caee8f057c1d31
Odaymon <stevenpascaran@gmail.com>
2019-03-03 15:32:30 +0800

    testing server.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -21,7 +21,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|scss)$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
 
