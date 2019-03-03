c97bde661c11a2d216758558aa694f516b6e134a
Odaymon <stevenpascaran@gmail.com>
2019-03-03 12:11:32 +0800

    testing server.js

--- src/server.js
+++ src/server.js
@@ -2,10 +2,23 @@ const express = require('express');
 const PORT = process.env.PORT || 8080;
 const path = require('path');
 
-express().get('/', (req, res) => {
-  res.send('root route');
+//express().get('/', (req, res) => {
+//  res.send('root route');
+//});
+
+const http = require('http'),
+      server = http.createServer();
+
+server.on('request',(request,response)=>{
+   response.writeHead(200,{'Content-Type':'text/plain'});
+   response.write('Hello world');
+   response.end();
 });
 
-express().listen(PORT, (res,req) => {
+server.listen(PORT, (res,req) => {
 	console.log(`Listening on ${ PORT }`)
 });
+
+//express().listen(PORT, (res,req) => {
+//	console.log(`Listening on ${ PORT }`)
+//});
